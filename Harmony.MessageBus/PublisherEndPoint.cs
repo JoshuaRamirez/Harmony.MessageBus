0b6f67a0d278fb816ad2e5d7bf434df45e2035ba
Joshua Ramirez <joshuaramirezdeveloper@gmail.com>
2012-04-26 23:34:57 -0500

    Added some generic publishing from an endpoint. Not sure where I'm going with this exactly. I'll figure it out later.

---
+++ Harmony.MessageBus/PublisherEndPoint.cs
@@ -0,0 +1,13 @@
+﻿using Tally.Bus.Core;
+
+namespace Tally.Bus
+{
+    public class PublisherEndpoint<TEventMessage> : IEventPublisher<TEventMessage> where TEventMessage : IEventMessage
+    {
+        public event HarmonyEvent<TEventMessage> Published;
+        public void OnPublished(TEventMessage eventMessage)
+        {
+            if (Published != null) Published(eventMessage);
+        }
+    }
+}
