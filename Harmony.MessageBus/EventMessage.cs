46a01dcee389845684c5760325d546ef9852ecbb
Joshua Ramirez <joshuaramirezdeveloper@gmail.com>
2012-04-26 23:44:31 -0500

    Added an abstract event. I'm starting to use the library in TapSumo and any type of frameworky code I write in the client library will get moved to Harmony.MessageBus

---
+++ Harmony.MessageBus/EventMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Tally.Bus.Core;
+
+namespace Tally.Bus
+{
+    public abstract class EventMessage : IEventMessage
+    {
+        private readonly Guid _messageId;
+
+        protected EventMessage(Guid messageId)
+        {
+            _messageId = messageId;
+        }
+
+        public Guid MessageId
+        {
+            get { return _messageId; }
+        }
+    }
+}
