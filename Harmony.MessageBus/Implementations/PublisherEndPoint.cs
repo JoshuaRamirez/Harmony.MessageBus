bbf216617d0f1f62e2869f8e78f45b1a03558935
Joshua Ramirez <joshuaramirezdeveloper@gmail.com>
2012-05-07 22:36:27 -0500

    Added concept of endpoints. Still experimenting. Fixed a namespace artifact.

---
+++ Harmony.MessageBus/Implementations/PublisherEndPoint.cs
@@ -0,0 +1,13 @@
+﻿using Harmony.MessageBus.Core;
+
+namespace Harmony.MessageBus.Implementations
+{
+    public abstract class PublisherEndpoint<TEventMessage> : IEventPublisher<TEventMessage> where TEventMessage : IEventMessage
+    {
+        public event HarmonyEvent<TEventMessage> Published;
+        protected void Publish(TEventMessage eventMessage)
+        {
+            if (Published != null) Published(eventMessage);
+        }
+    }
+}
---
+++ Harmony.MessageBus/Implementations/SubscriberEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Harmony.MessageBus.Core;
+
+namespace Harmony.MessageBus.Implementations
+{
+    public abstract class SubscriberEndpoint<TEventMessage> : IEventSubscriber<TEventMessage> where TEventMessage : IEventMessage
+    {
+
+        private readonly Guid _subscriberId;
+
+        protected SubscriberEndpoint()
+        {
+            _subscriberId = Guid.NewGuid();
+        }
+
+        internal SubscriberEndpoint(Guid subscriberId)
+        {
+            _subscriberId = subscriberId;
+        }
+
+        public Guid Id
+        {
+            get { return _subscriberId; }
+        }
+
+        public abstract void Handle(TEventMessage eventMessage);
+    }
+
+    public class SubscriberEndpoint
+    {
+
+    }
+}
